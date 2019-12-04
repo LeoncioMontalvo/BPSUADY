707278acb29b83799d7c162b6870c2ce0bed4702
Isaac Canche <icanche83@gmail.com>
2019-12-04 15:41:03 -0600

    conflictos arreglados

--- BPSUADY/plantillas/piePagina.php
+++ BPSUADY/plantillas/piePagina.php
@@ -3,15 +3,11 @@
         <div class="contacto">
             <h2>Contacto</h2>
             <ul>
-<<<<<<< HEAD
-                <li id="lista">Facultad de Matematicas UADY</li> <br>
-                <li id="lista">+52 12345678</li> <br>
-                <li id="lista">bpsuadycontacto@gmail.com</li>
-=======
+
                 <li class="lista">Facultad de Matematicas UADY</li>
                 <li class="lista">+52 12345678</li>
                 <li class="lista">bpsuadycontacto@gmail.com</li>
->>>>>>> 10de2378d85dd2c1f838a2296eff53fbc27ac148
+
             </ul>
         </div>
         <div class="informacion">
