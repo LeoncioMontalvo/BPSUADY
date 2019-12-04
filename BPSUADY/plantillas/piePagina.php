10de2378d85dd2c1f838a2296eff53fbc27ac148
Leoncio Montalvo Leon <Leo_239@hotmail.es>
2019-12-04 12:16:44 -0600

    Mejoras en el footer

--- BPSUADY/plantillas/piePagina.php
+++ BPSUADY/plantillas/piePagina.php
@@ -3,9 +3,9 @@
         <div class="contacto">
             <h2>Contacto</h2>
             <ul>
-                <li id="lista">Facultad de Matematicas UADY</li>
-                <li id="lista">+52 12345678</li>
-                <li id="lista">bpsuadycontacto@gmail.com</li>
+                <li class="lista">Facultad de Matematicas UADY</li>
+                <li class="lista">+52 12345678</li>
+                <li class="lista">bpsuadycontacto@gmail.com</li>
             </ul>
         </div>
         <div class="informacion">
@@ -13,9 +13,9 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae facilis necessitatibus consequat</p>
             
             <div id="iconos">
-                <a id="icono" href="#"><span>Facebook</span></a>
-                <a id="icono" href="#"><span>Twitter</span></a>
-                <a id="icono" href="#"><span>Github</span></a>
+                <a class="icono" href="#"><span>Facebook</span></a>
+                <a class="icono" href="#"><span>Twitter</span></a>
+                <a class="icono" href="#"><span>Github</span></a>
             </div>
         </div>
     </div>
